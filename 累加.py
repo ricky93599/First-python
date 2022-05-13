8c3afa642b7deff4f9aa5545a9fcbabbfdca2f25
ricky93599 <105497236+ricky93599@users.noreply.github.com>
2022-05-13 20:32:09 +0800

    Add files via upload

---
+++ 累加.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 20:28:02 2022
+
+@author: rrr93
+"""
+def add (x):
+    v = 0
+    for i in range(1,x+1):
+        v +=i
+        return v
+    
+    z = add(10)
+    print(z)
+    
